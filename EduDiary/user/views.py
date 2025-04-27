c74999d68702293480bca6937d17d5b63ba30e9a
Vale <119837612+ValeZask@users.noreply.github.com>
2025-04-27 20:24:04 +0600

    Revert "UserRegister"

--- EduDiary/user/views.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import generics
-from .serializers import RegisterSerializer
-from django.contrib.auth.models import User
-
-class RegisterView(generics.CreateAPIView):
-    queryset = User.objects.all()
-    serializer_class = RegisterSerializer
