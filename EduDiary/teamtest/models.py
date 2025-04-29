9aabd1147afda1f1793b72c9bfedea9bdff7a277
ТВОЁ_ИМЯ_В_GIT <_EMAIL>
2025-04-29 19:49:05 +0600

    just testing

--- EduDiary/teamtest/models.py
+++ EduDiary/teamtest/models.py
@@ -2,3 +2,8 @@ from django.db import models
 
 # Create your models here.
 # Hello World
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    published_year = models.IntegerField()
+    
